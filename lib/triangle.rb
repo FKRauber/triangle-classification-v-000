504629f44a97bb144318e43125ff7ec12f8a15d8
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-29 14:14:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,19 @@
 class Triangle
   # write code here
   attr_reader :a, :b, :c
+
   def initialize(a, b, c)
     @a = a
     @b = b
     @c = c
+  end
 
+  def kind
+
+  end
+  def validate_triangle
+    true_tri = [(a+b>c), (a+c>b), (b+c>a)]
+    [a, b, c].each { |side| true_tri << false if side <= 0 }
+    raise TriangleError if true_tri.include?(false)
   end
 end
