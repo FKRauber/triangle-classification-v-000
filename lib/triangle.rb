8a1b854b9ca87152f6dd67b1b7ce369beab749cb
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-29 14:16:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,14 @@ class Triangle
   end
 
   def kind
-
+    validate_triangle
+    if a == b && b == c 
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
   end
   def validate_triangle
     true_tri = [(a+b>c), (a+c>b), (b+c>a)]
